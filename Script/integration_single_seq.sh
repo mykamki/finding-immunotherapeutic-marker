993774f3a190c78cdad8f62b942da5bb42a2d9bf
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 17:56:29 +0900

    Update integration_single_seq.sh

--- Script/integration_single_seq.sh
+++ Script/integration_single_seq.sh
@@ -1,7 +1,20 @@
+# Integration using SCT
+# Integration goals
+# 1. Create an ‘integrated’ data assay for downstream analysis
+# 2. Identify cell types that are present in both datasets
+# 3. Obtain cell type markers that are conserved in both control and stimulated cells
+# 4. Compare the datasets to find cell-type specific responses to stimulation
 
 
+# 01. Integration 
+Rscript integration_single_seq_by_SCT.R ../Output/ ../Output/INTEGRATION/
 
 
-quality_control_single_seq_find_optimal_resolution.R ../Output/INTEGRATION/ 10
+# 02. Check optimal resolution for clustering
+Rscript quality_control_single_seq_find_optimal_resolution.R ../Output/INTEGRATION/ 30
+
+
+# 03. Do clustering
+Rscript 2_INTEGRATION/integ05_clustering.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/ 30 1 ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/
 
 
