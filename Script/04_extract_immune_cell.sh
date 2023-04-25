79985a4ce1cc6ff9bfe9a56700cf9191a8e02515
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 18:17:54 +0900

    Create 04_extract_immune_cell.sh

---
+++ Script/04_extract_immune_cell.sh
@@ -0,0 +1,19 @@
+# annotation marker to integrated dataset
+# outdir is "cell_type_ann_result"
+Rscript 3_MARKER/marker01_vlnplot.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/
+
+# mannually
+vim ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/cell_type_ann_result/cluster_ann.R 
+
+# re-assign cell type using cell-marker genes
+mkdir ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1 
+Rscript 3_MARKER/marker02_reassign_celltype.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/ ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/cell_type_ann_result/cluster_ann.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1/ 
+
+# split data for purpose
+mkdir ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/rm_epi
+Rscript 4_SUBSET/subset01_rm_epithelial.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1/ ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/rm_epi/  
+
+####################################################
+#### Before removing  Epithelial_cell  :  36300 ####
+##### After removing  Epithelial_cell  :  1517 #####
+####################################################
