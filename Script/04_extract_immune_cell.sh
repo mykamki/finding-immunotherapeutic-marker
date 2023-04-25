ae21480c0184e18e24135b132d75ccc3010d597c
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 18:31:44 +0900

    Update 04_extract_immune_cell.sh

--- Script/04_extract_immune_cell.sh
+++ Script/04_extract_immune_cell.sh
@@ -3,17 +3,14 @@
 Rscript make_violin_plot_using_cell_markers.R ../Output/INTEGRATION/
 
 # mannually
-vim ../Output/INTEGRATION//cell_type_ann_result/cluster_ann.R 
+vim ../Output/INTEGRATION/cell_type_ann_result/cluster_ann.R 
 
-# re-assign cell type using cell-marker genes
-mkdir ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1 
-Rscript 3_MARKER/marker02_reassign_celltype.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/ ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/cell_type_ann_result/cluster_ann.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1/ 
-
-# split data for purpose
-mkdir ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/rm_epi
-Rscript 4_SUBSET/subset01_rm_epithelial.R ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/anned_data1/ ~/ANALYSIS/singlecell/Rdata/integ_3000_mt/dim30/rm_epi/  
+# re-assign cell type using cell-marker genes and extract just immune cells
+mkdir ../Output/INTEGRATION/IMMUNE/
+Rscript remove_specific_cell_type.R ../Output/INTEGRATION/../Output/INTEGRATION/cell_type_ann_result/cluster_ann.R Epithelial_cell ../Output/INTEGRATION/IMMUNE/
 
 ####################################################
 #### Before removing  Epithelial_cell  :  36300 ####
 ##### After removing  Epithelial_cell  :  1517 #####
 ####################################################
+
