ee6cefd219640e2ddcb8935a29b0b2a765ce1e05
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 17:26:13 +0900

    Update preprocessing_single_cell_dataset.sh

--- Script/preprocessing_single_cell_dataset.sh
+++ Script/preprocessing_single_cell_dataset.sh
@@ -3,18 +3,21 @@
 # 1. make dataset and check nFeature for determine minimal cell counts
 for n in {1..7};do Rscript quality_control_single_seq_check_feautres.R ../Data/ SC${n} ../Output/SC${n]/; done
 
+
 # 2. input hyper-parameter for determine minimal cell counts
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC1/ BC1 200 7500 10 ~/ANALYSIS/singlecell/Rdata/sc1/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC2/ BC2 1000 6000 10 ~/ANALYSIS/singlecell/Rdata/sc2/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC3/ BC3 500 6000 10 ~/ANALYSIS/singlecell/Rdata/sc3/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC4/ BC4 700 3000 10 ~/ANALYSIS/singlecell/Rdata/sc4/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC5/ BC5 500 4000 10 ~/ANALYSIS/singlecell/Rdata/sc5/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC6/ BC6 500 5000 7.5 ~/ANALYSIS/singlecell/Rdata/sc6/
-Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC7/ BC7 400 4000 10 ~/ANALYSIS/singlecell/Rdata/sc7/
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC1/ BC1 200 7500 10 
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC2/ BC2 1000 6000 10 
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC3/ BC3 500 6000 10 
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC4/ BC4 700 3000 10 
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC5/ BC5 500 4000 10 
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC6/ BC6 500 5000 7.5
+Rscript quality_control_single_seq_subset_and_normalization.R ../Output/SC7/ BC7 400 4000 10 
+
 
 # 3. input hyper-parameter for pca and find optimal resolution
 for n in {1..7};do Rscript 1_QC/qc03_find_optimal_resolution.R ~/ANALYSIS/singlecell/Rdata/sc$n/ 10 ~/ANALYSIS/singlecell/Rdata/sc$n/; done
 Rscript 1_QC/qc03_find_optimal_resolution.R ~/ANALYSIS/singlecell/Rdata/sc5/ 15 ~/ANALYSIS/singlecell/Rdata/sc5/
+
 
 # 4. input hyper-parameter for clustering
 Rscript 1_QC/qc04_clustering.R ~/ANALYSIS/singlecell/Rdata/sc1/ 10 1.6 ~/ANALYSIS/singlecell/Rdata/sc1/  
