3d8dd3904263fd3edc1ac01a34d60fa4c49a7366
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 17:10:37 +0900

    Update quality_control_single_seq_clustering.R

--- Script/quality_control_single_seq_clustering.R
+++ Script/quality_control_single_seq_clustering.R
@@ -4,21 +4,24 @@ library(Seurat)
 library(cluster, quietly = TRUE)
 library(dplyr)
 indir <- args[1]
-
-
-
-#### 01. Load data ####
-load(file = paste0(args[1], "data1.RData"))
-
-
-#### 02. Cluster the cells ####
 max.dim <- as.double(args[2])
 opt.resol <- as.double(args[3])
+outdir <- args[4]
+
+
+
+### 01. Load data
+load(file = paste0(indir, "normalized_data1.RData"))
+
+
+
+### 02. Cluster the cells
 data1 <- FindNeighbors(data1, dims = 1:max.dim) # default dims_Neighbors = 1:10
 data1 <- FindClusters(data1, resolution = opt.resol)
 
 
-#### 03. Run non-linear dimensional reduction (UMAP/tSNE) ####
+
+### 03. Run non-linear dimensional reduction (UMAP/tSNE)
 # Seurat offers several non-linear dimensional reduction techniques, such as tSNE and UMAP, to visualize and explore these datasets. 
 # The goal of these algorithms is to learn the underlying manifold of the data in order to place similar cells together in low-dimensional space. 
 # Cells within the graph-based clusters determined above should co-localize on these dimension reduction plots. 
@@ -27,12 +30,17 @@ data1 <- RunTSNE(object = data1, dims = 1:max.dim)
 data1 <- RunUMAP(object = data1, dims = 1:max.dim)
 
 
-save(data1, file = paste0(args[4], "preprocessed_data1.RData"))
 
-
-#### 04. Plot ####
+### 04. Plot umap 
 set.seed(12345)
-
-pdf(file = paste0(args[4], "umap.pdf"))
+pdf(file = paste0(outdir, "umap.pdf"))
 DimPlot(data1, reduction = "umap")
 dev.off()
+
+
+
+#### 05. Save the results
+save(data1, file = paste0(outdir, "preprocessed_data1.RData"))
+
+
+
