26e84a0645307af2f3d1e2e74910db92223b00d9
mykamki <125241834+mykamki@users.noreply.github.com>
2023-04-25 18:34:15 +0900

    Update 01_prerpocessing_bulk_dataset.sh

--- Script/01_prerpocessing_bulk_dataset.sh
+++ Script/01_prerpocessing_bulk_dataset.sh
@@ -1,5 +1,5 @@
 # prerpocessing bulk rna-seq
-Rscript preprocessing_bulk_seq_clinical.R ../Data/ ../Output/BULK/
-Rscript preprocessing_bulk_seq_tpm.R ../Data/ ../Output/BULK/
+Rscript preprocessing_bulk_seq.R ../Data/ ../Output/BULK/
+
 
 
