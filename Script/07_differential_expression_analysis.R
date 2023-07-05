9290089480f7b0546417c58b6b953c90eded8bae
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 16:06:00 +0900

    Create 07_differential_expression_analysis.R

---
+++ Script/07_differential_expression_analysis.R
@@ -0,0 +1,5 @@
+
+myident1 <- "neg_scissor"
+others <- c("T_cell", "B_cell", "Myeloid_macrophage", "Fibroblast", "other", "Endothelial_cell")  
+de.neg <- FindMarkers(data1, ident.1 = myident1, ident.2 = others, test.use = "DESeq2")
+de.neg <- test_de_cutoff(de.neg, 1.5)
