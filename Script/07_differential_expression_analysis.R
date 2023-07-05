76f84ce7c701c1de5b863c949aa0f371a17b348d
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 23:56:35 +0900

    Update 07_differential_expression_analysis.R

--- Script/07_differential_expression_analysis.R
+++ Script/07_differential_expression_analysis.R
@@ -10,6 +10,9 @@ library(dplyr)
 library(DESeq2)
 library(ggplot2)
 library(ggrepel)
+library(ggsignif)
+library(grid)
+library(gridExtra)
 
 
 
@@ -42,8 +45,137 @@ save(de.neg, file = paste0(indir, "negcell.de.result.RData"))
 write.csv(de.neg, file = paste0(indir, "scissor_negative_de_result.csv"), row.names = F, quote = F)
 
 
-### 06. Vocano plot
+
+### 06. Volcano plot
+volcano_p <- my_volcano(de.neg)
+
+png(file = paste0(indir, "scissor_negative_de_vocanoplot.png"), width = 500, height= 500)
+volcano_p
+dev.off()
 
 
+### 07. DE gene Violin plot
+###...07-1. make de gene list 
+de.neg %>% filter(expression == "Up-regulated") %>% rownames() -> degenes
 
+# "SSR4" "CD74" "HLA-DPA1" "JCHAIN" "HLA-DRA"  "RGS1"
+# "SSR4" "RGS1" "HLA-DRB5" "APOE" "C1QB"  "C1QA"  "APOC1"  "JCHAIN"  "C1QC"  "DERL3"   
+
+degenes <- degenes[grep("IG", degenes, invert = T)]
+
+###...07-2. make group (scissor neg cell vs. others) 
+cell_group_df <- as.data.frame(Idents(data1))
+colnames(cell_group_df) <- "celltype"
+cell_group_df$group1 <- ifelse(cell_group_df$celltype == "neg_scissor", 1, 0)
+cell_group_df$group2 <- ifelse(cell_group_df$celltype == "pos_scissor", 1, 0)
+
+data1[["group1"]] <- cell_group_df$group1 # create cell names as metadata colum
+data1[["group2"]] <- cell_group_df$group2 # create cell names as metadata colum
+data1[["group3"]] <- ifelse(cell_group_df$group1 == 1, 2, 
+												ifelse(cell_group_df$group2 == 1, 1, 0))
+
+#table(data1[["group3"]])
+#group3
+#   0    1    2 
+#1229  173  115 
+
+###...07-3. remove scissor + cells
+data_group <- subset(data1, idents = "pos_scissor", invert = TRUE)
+
+###...07-4. plot of de genes 
+my_vlnplot_for_neg_degene <- function(dt, gene) {
+  VlnPlot(dt , features = gene , group.by = "group1",pt.size = 0, y.max = 8) +
+  theme(legend.position = "top") +
+  scale_fill_manual(values = c('grey','royalblue'), 
+    labels = c("Others", "Scissor- cells")) +
+  geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5) +
+  theme(axis.title.x=element_blank()) + 
+  scale_x_discrete(labels = c("",""))
+}
+
+# "SSR4" "RGS1" "HLA-DRB5" "APOE" "C1QB"  "C1QA"  "APOC1"  "JCHAIN"  "C1QC"  "DERL3"   
+
+p1 <- my_vlnplot_for_neg_degene(data_group, "SSR4")
+p2 <- my_vlnplot_for_neg_degene(data_group, "RGS1")
+p3 <- my_vlnplot_for_neg_degene(data_group, "HLA-DRB5")
+p4 <- my_vlnplot_for_neg_degene(data_group, "APOE")
+p5 <- my_vlnplot_for_neg_degene(data_group, "C1QB")
+
+p6 <- my_vlnplot_for_neg_degene(data_group, "C1QA")
+p7 <- my_vlnplot_for_neg_degene(data_group, "APOC1")
+p8 <- my_vlnplot_for_neg_degene(data_group, "JCHAIN")
+p9 <- my_vlnplot_for_neg_degene(data_group, "C1QC")
+p10 <- my_vlnplot_for_neg_degene(data_group, "DERL3")
+
+
+# Create user-defined function, which extracts legends from ggplots
+extract_legend <- function(my_ggp) {
+  step1 <- ggplot_gtable(ggplot_build(my_ggp))
+  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
+  step3 <- step1$grobs[[step2]]
+  return(step3)
+}
+
+shared_legend <- extract_legend(p1)
+
+png(
+grid.arrange(
+p1 + theme(legend.position='hidden'),
+p2 + theme(legend.position='hidden'),
+p3 + theme(legend.position='hidden'),
+p4 + theme(legend.position='hidden'),
+p5 + theme(legend.position='hidden'),
+p6 + theme(legend.position='hidden'),
+p7 + theme(legend.position='hidden'),
+p8 + theme(legend.position='hidden'),
+p9 + theme(legend.position='hidden'),
+p10 + theme(legend.position='hidden'), 
+  top = shared_legend$grobs[[1]])
+
+
+  (pF1,pF2,pF3,pF4,pF5,pF6, bottom =shared_legend$grobs[[1]], 
+ncol =3, nrow = 2)
+
+                        
+
+pF1 <- VlnPlot(data2 , features="JCHAIN" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5)+
+theme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
+
+pF2 <- VlnPlot(data2 , features="SSR4" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5)+
+theme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
+
+pF3 <- VlnPlot(data2 , features="CD74" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5)+
+theme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
+
+pF4 <- VlnPlot(data2 , features="HLA-DPA1" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5)+
+theme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
+
+pF5 <- VlnPlot(data2 , features="HLA-DRA" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+geom_signif(comparisons = list(c("0","1")),map_signif_level = TRUE, y_position =7.5)+
+theme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
+
+pF6 <- VlnPlot(data2 , features="RGS1" , group.by = "group1",pt.size = 0, y.max = 8)+
+theme(legend.position = "bottom")+scale_fill_manual(values = c('grey','royalblue'), 
+labels = c("others", "Scissor- cells"))+
+ㄴtheme(axis.title.x=element_blank()) + 
+scale_x_discrete(labels = c("",""))
 
