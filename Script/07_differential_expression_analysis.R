f2d9f3ee08eeb50b589b91c98bda4e6349cbdea8
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 16:12:46 +0900

    Update 07_differential_expression_analysis.R

--- Script/07_differential_expression_analysis.R
+++ Script/07_differential_expression_analysis.R
@@ -1,10 +1,28 @@
+#!/usr/bin/env Rscript
+# 07_differential_expression_analysis.R $INDIR 
 
+
+### 01. Setting
+source("Script/Functions/func_exploring.R")
+args <- commandArgs(trailingOnly = TRUE)
 library(Seurat)
 library(dplyr)
 library(DESeq2)
 
 
-### 
+### 02. Load data
+load(paste0(indir, "infos1.RData"))
+load(paste0(indir, "sc_dataset.RData"))
+
+
+
+### 03. Extract Scissor cells
+pos_cell_names <- infos1$Scissor_pos
+neg_cell_names <- infos1$Scissor_neg
+
+
+
+### 04. Identify 
  Idents(data1)
 
 myident1 <- "neg_scissor"
