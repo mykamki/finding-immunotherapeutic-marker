a5a0c9a107e64ae9f37487703323d47d8d07c456
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 13:08:30 +0900

    Update 05_run_scissor.sh

--- Script/05_run_scissor.sh
+++ Script/05_run_scissor.sh
@@ -5,28 +5,24 @@ Rscript run_scissor.R ../Output/BULK/ ../Output/INTEGRATION/IMMUNE/preprocessed_
 #[1] "|**************************************************|"
 #[1] "Performing quality-check for the correlations"
 #[1] "The five-number summary of correlations:"
-#        0%        25%        50%        75%       100%
-#0.05230147 0.22688970 0.31980264 0.35015502 0.58440731
+#       0%       25%       50%       75%      100% 
+#0.1239261 0.3059452 0.3404038 0.3746918 0.6112234 
 #[1] "|**************************************************|"
 #[1] "Perform cox regression on the given clinical outcomes:"
 #[1] "alpha = 0.005"
-#[1] "Scissor identified 263 Scissor+ cells and 730 Scissor- cells."
-#[1] "The percentage of selected cell is: 65.458%"
+#[1] "Scissor identified 361 Scissor+ cells and 665 Scissor- cells."
+#[1] "The percentage of selected cell is: 67.633%"
 
 #[1] "alpha = 0.01"
-#[1] "Scissor identified 273 Scissor+ cells and 556 Scissor- cells."
-#[1] "The percentage of selected cell is: 54.647%"
+#[1] "Scissor identified 312 Scissor+ cells and 456 Scissor- cells."
+#[1] "The percentage of selected cell is: 50.626%"
 
 #[1] "alpha = 0.05"
-#[1] "Scissor identified 233 Scissor+ cells and 259 Scissor- cells."
-#[1] "The percentage of selected cell is: 32.432%"
+#[1] "Scissor identified 252 Scissor+ cells and 177 Scissor- cells."
+#[1] "The percentage of selected cell is: 28.279%"
 
 #[1] "alpha = 0.1"
-#[1] "Scissor identified 174 Scissor+ cells and 163 Scissor- cells."
-#[1] "The percentage of selected cell is: 22.215%"
-
-#[1] "alpha = 0.2"
-#[1] "Scissor identified 57 Scissor+ cells and 77 Scissor- cells."
-#[1] "The percentage of selected cell is: 8.833%"
+#[1] "Scissor identified 173 Scissor+ cells and 115 Scissor- cells."
+#[1] "The percentage of selected cell is: 18.985%"
 #[1] "|**************************************************|"
 
