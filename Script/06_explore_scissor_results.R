4150dc99b7b1f26a038f22b53fe11eb24c02514f
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 13:36:49 +0900

    Update 06_explore_scissor_results.R

--- Script/06_explore_scissor_results.R
+++ Script/06_explore_scissor_results.R
@@ -1,1 +1,17 @@
-r
+#!/usr/bin/env Rscript
+# 06_explore_scissor_results.R $INDIR 
+
+#### 01.
+args <- commandArgs(trailingOnly = TRUE)
+library(Scissor)
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+
+
+#### 01. Load data ####
+location <- args[1]
+load(paste0(location, "infos1.RData"))
+load(paste0(location, "preprocessed_data1.RData"))
+
+
