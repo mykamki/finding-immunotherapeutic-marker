6b55d8fbaecfe54b1b978932ce84b726c7848fbd
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-05 16:10:46 +0900

    Update 07_differential_expression_analysis.R

--- Script/07_differential_expression_analysis.R
+++ Script/07_differential_expression_analysis.R
@@ -1,4 +1,12 @@
 
+library(Seurat)
+library(dplyr)
+library(DESeq2)
+
+
+### 
+ Idents(data1)
+
 myident1 <- "neg_scissor"
 others <- c("T_cell", "B_cell", "Myeloid_macrophage", "Fibroblast", "other", "Endothelial_cell")  
 de.neg <- FindMarkers(data1, ident.1 = myident1, ident.2 = others, test.use = "DESeq2")
