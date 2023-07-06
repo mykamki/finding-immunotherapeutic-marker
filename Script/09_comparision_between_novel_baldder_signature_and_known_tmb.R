835e08236c9f9a693f49dd645fab8923e21eea8f
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-06 14:37:56 +0900

    Update 09_comparision_between_novel_baldder_signature_and_known_tmb.R

--- Script/09_comparision_between_novel_baldder_signature_and_known_tmb.R
+++ Script/09_comparision_between_novel_baldder_signature_and_known_tmb.R
@@ -7,12 +7,13 @@ outdir <- args[2]
 
 ### 02. Load data
 load(paste0(indir,"clinical_gse176307.RData"))
-load(paste0(indir,"clinical_gse176307.RData"))
-load(paste0(indir,"clinical_gse176307.RData"))
+load(paste0(indir,"clinical_imvigor210core.RData"))
+load(paste0(indir,"clinical_ucgenome.RData"))
 
 
 
 ### 03. Survival relevance and ICB response of novel bladder signature in GSE176307
+
 
 
 ### 04. Survival relevance and ICB response of novel bladder signature in IMvigor210
