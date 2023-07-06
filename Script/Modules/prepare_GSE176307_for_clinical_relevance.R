e5d256d5993d1dfb73de49319f57ef6e9b5ec12b
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-06 10:45:39 +0900

    Update prepare_GSE176307_for_clinical_relevance.R

--- Script/Modules/prepare_GSE176307_for_clinical_relevance.R
+++ Script/Modules/prepare_GSE176307_for_clinical_relevance.R
@@ -1,6 +1,8 @@
 ### 01. Setiing
 library(limma)
 library(dplyr)
+indir <- args[1]
+outdir <- args[2]
 
 
 ### 02. Load data
@@ -38,7 +40,9 @@ clinical_gse176307$ecog <- ifelse(clinical_gse176307$ecog == "0", 0,
 
 
 ### 03. preprocessing bulk dataset for bladder signature
-mygene <- c("SSR4", "CD74", "HLA-DPA1", "JCHAIN", "HLA-DRA", "RGS1")
+### ...03-1. make normalized zscore
+#mygene <- c("SSR4", "CD74", "HLA-DPA1", "JCHAIN", "HLA-DRA", "RGS1")
+mygene <- c("SSR4", "RGS1" ,"HLA-DRB5", "APOE", "C1QB",  "C1QA",  "APOC1",  "JCHAIN",  "C1QC", "DERL3")
 zscore_transform <- function(x) {
   (x-mean(x))/sd(x)
 }
@@ -47,11 +51,25 @@ log_dataset <- v$E
 log_dataset2 <- apply(log_dataset, 2, zscore_transform)
 
 
+### ...03-2. divide group by bladder signature
+mygene <- ifelse(mygene %in% "JCHAIN", "IGJ", mygene)
+rownames(log_dataset2) %in% mygene %>% sum() # check gene names
+res2 <- apply(log_dataset2[rownames(log_dataset2) %in% mygene ,],2,mean)
+sumgsig <- summary(res2)
+names(res2)[which(res2<=sumgsig[3])] -> low # low ID
+names(res2)[which(res2>sumgsig[3])] -> high # high ID
 
-# check whether bulk_dataset colname matched with phenotype ID
-if (identical(phenotype$ID , colnames(bulk_dataset))) {
-  bulk_dataset <- bulk_dataset[,phenotype$ID]
-}
-rownames(phenotype) <- phenotype$ID
-phenotype <- phenotype[,c("time","status")]
+clinical_gse176307 <- clinical_gse176307 %>% mutate(Novel_Signature = ifelse(ID %in% high, "High", "Low"))
+clinical_gse176307$Novel_Signature <- factor(clinical_gse176307$Novel_Signature)
+
+if (identical(clinical_gse176307$ID, names(res2))) {
+	res2 <- res2[clinical_gse176307$ID]
+	}
+clinical_gse176307$Novel_Signature_score <- res2
+
+
+### 04. Save data
+log_dataset_gse176307 <- log_dataset2
+save(clinical_gse176307, file = paste0(outdir, "clinical_gse176307.RData"))
+save(log_dataset_gse176307, file = paste0(outdir, "log_dataset_gse176307.RData"))
 
