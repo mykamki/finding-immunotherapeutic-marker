a2bfee519825e9dc097142d67e4ba07eec16ff33
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-06 10:48:25 +0900

    Update 08_prepare_datasets_for_evaluation.sh

--- Script/08_prepare_datasets_for_evaluation.sh
+++ Script/08_prepare_datasets_for_evaluation.sh
@@ -1,1 +1,5 @@
-Rscript Modules/prepare.R $indir $outdir
+
+Rscript Modules/prepare_GSE176307_for_clinical_relevance.R $indir $outdir
+Rscript Modules/prepare_IMvigor210_for_clinical_relevance.R $indir $outdir
+Rscript Modules/prepare_UC-GENOME_for_clinical_relevance.R $indir $outdir
+
