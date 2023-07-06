60df02a0c6f098526727707c5b3342f80d6c71dc
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-06 10:57:14 +0900

    Update func_clinical_relevance.R

--- Script/Functions/func_clinical_relevance.R
+++ Script/Functions/func_clinical_relevance.R
@@ -1,1 +1,10 @@
 
+tpm <- function(counts, lengths) {
+	rpk <- counts/(lengths/1000) # 1. rpk
+	coef <- sum(rpk)/1e6 # 2. coef(per million scaling factor)
+	rpk/coef # 3. tpm
+}
+
+zscore_transform <- function(x) {
+  (x-mean(x))/sd(x)
+}
