2e97b9e695c6abc669a0162d1d772b40daf290f0
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-17 13:39:50 +0900

    Update 10_combination_between_novel_baldder_signature_and_known_tmb.R

--- Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
+++ Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
@@ -136,18 +136,18 @@ bar_plot_by_combination <- function(dataset) {
 	return(p)
 }
 
-bar_plot_by_extreme_combination <- function(dataset) {
-	dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HH", "LL"))
+bar_plot_by_combination <- function(dataset) {
+	#dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HH", "LL"))
 	data <- dataset %>% 
                 filter(!is.na(response.binary)) %>% 
                 group_by(Combination_Signature_TMB, response.binary) %>% 
                 summarize(n = n())
         data$response.binary <- factor(data$response.binary, levels = c("R", "NR"))
 	
-	hh <- sum(data[data$Combination_Signature_TMB == "HH",]$n)
+	hh <- sum(data[data$Combination_Signature_TMB == "Any",]$n)
 	ll <- sum(data[data$Combination_Signature_TMB == "LL",]$n)
 
-	data$tt <- c(rep(hh,nrow(data[data$Combination_Signature_TMB == "HH",])), 
+	data$tt <- c(rep(hh,nrow(data[data$Combination_Signature_TMB == "Any",])), 
 		     rep(ll,nrow(data[data$Combination_Signature_TMB == "LL",])))
 	data <- data %>% mutate(percent = round(n/tt*100,1)) 
 
@@ -158,9 +158,9 @@ bar_plot_by_extreme_combination <- function(dataset) {
         # make plot
         p <- ggplot(data, aes(fill=response.binary, y=percent, x=Combination_Signature_TMB)) +
                 geom_col() + 
-                geom_signif(comparisons = list(c("HH", "LL")), 
+                geom_signif(comparisons = list(c("Any", "LL")), 
                             annotations = paste0("pvalue=", round(dtt$p.value,2)), y_position = 102) +
-                scale_x_discrete(labels=c(paste0("HH\n(n=", hh,")"), 
+                scale_x_discrete(labels=c(paste0("Any\n(n=", hh,")"), 
 					  paste0("LL\n(n=", ll,")"))) +
                 scale_fill_manual(values = c("orange", "royalblue3")) +
                 xlab("") + ylab("") + 
@@ -221,23 +221,6 @@ p3b <- bar_plot_by_combination(clinical_ucgenome)
 
 
 
-clinical_gse176307 <- make_combination_group_dataset(clinical_gse176307)
-clinical_imvigor210core <- make_combination_group_dataset(clinical_imvigor210core)
-clinical_ucgenome <- make_combination_group_dataset(clinical_ucgenome)
-p1a <- my_combination_survival_plot(clinical_gse176307, "GSE176307")
-p2a <- my_combination_survival_plot(clinical_imvigor210core, "IMvigor210")
-p3a <- my_combination_survival_plot(clinical_ucgenome, "UC-GENOME")
-
-p <- grid.arrange(
-grid.arrange(p1a$plot + theme(legend.position='hidden'),p1a$table, layout_matrix = rbind(c(1), c(1), c(2))),
-grid.arrange(p2a$plot + theme(legend.position='hidden'),p2a$table, layout_matrix = rbind(c(1), c(1), c(2))),
-grid.arrange(p3a$plot + theme(legend.position='hidden'),p3a$table, layout_matrix = rbind(c(1), c(1), c(2))),
-ncol = 1)
-
-png("test.png", width = 250, height = 800)
-grid.arrange(p, sur1_legend, heights = c(8, 1))
-dev.off()
-
 ### 06. Plotting
 pA <- grid.arrange(
 grid.arrange(p1a$plot + theme(legend.position='hidden'),p1a$table, layout_matrix = rbind(c(1), c(1), c(2))),
@@ -257,7 +240,9 @@ grid.arrange(pA, shared_legend, heights = c(8, 1))
 dev.off()
 
 
-
+png(paste0(outdir, "test.png"),  width = 400, height = 800)
+grid.arrange(pA, shared_legend, heights = c(8, 1))
+dev.off()
 
 my_HH_survival_plot <- function(dataset, datasettitle) {
 	dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HH", "LL"))
