6ee8f8c4f781d210350a28ab9d87807a25ac7ef3
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-19 14:49:24 +0900

    Update 10_combination_between_novel_baldder_signature_and_known_tmb.R

--- Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
+++ Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
@@ -14,10 +14,6 @@ source("Script/Functions/func_clinical_relevance.R")
 
 
 
-
-
-
-
 add_gene_exp <- function(log_dataset, dataset) {
 	genes <- c("SSR4", "RGS1", "HLA-DRB5", "APOE", "C1QB", "C1QA", "APOC1", "JCHAIN", "C1QC", "DERL3")
 	if (all(c("JCHAIN", "HLA-DRB5") %in% rownames(log_dataset))) {
@@ -49,10 +45,11 @@ add_gene_exp <- function(log_dataset, dataset) {
 	return(dataset)
 	}
 	
+#clinical_gse176307 <- add_gene_exp(log_dataset_gse176307, clinical_gse176307)
+#clinical_imvigor210core <- add_gene_exp(log_dataset_imvigor210core, clinical_imvigor210core)
+#clinical_ucgenome <- add_gene_exp(log_dataset_ucgenome, clinical_ucgenome)
 
 	
-
-
 
 ### 02. Load data
 load(paste0(indir,"clinical_gse176307.RData"))
@@ -81,10 +78,6 @@ p3a <- my_combination_survival_plot(clinical_ucgenome_2, "UC-GENOME")
 p3b <- bar_plot_by_combination(clinical_ucgenome_2)
 
 
-#clinical_gse176307 <- add_gene_exp(log_dataset_gse176307, clinical_gse176307)
-#clinical_imvigor210core <- add_gene_exp(log_dataset_imvigor210core, clinical_imvigor210core)
-#clinical_ucgenome <- add_gene_exp(log_dataset_ucgenome, clinical_ucgenome)
-
 ### 06. Plotting
 pA <- grid.arrange(
 grid.arrange(p1a$plot + theme(legend.position='hidden'),p1a$table, layout_matrix = rbind(c(1), c(1), c(2))),
@@ -98,28 +91,6 @@ ncol = 2)
 bar_legend <- extract_legend(p1b)
 sur1_legend <- extract_legend(p1a$plot)
 shared_legend <- grid.arrange(sur1_legend, bar_legend, nrow =1)
-			
-png(paste0(outdir, "fig3.png"),  width = 500, height = 800)
-grid.arrange(pA, shared_legend, heights = c(8, 1))
-dev.off()
-
-
-phh <- grid.arrange(pA, shared_legend, heights = c(8, 1))
-pll <- grid.arrange(pA, shared_legend, heights = c(8, 1))
-hh <- my_anno_plot("Both High vs. Any Low")
-ll <- my_anno_plot("Any High vs. Both Low")
-
-png(paste0(outdir, "test.png"),  width = 800, height = 900)
-grid.arrange(ggarrange(hh,ll,nrow =1), ggarrange(phh, pll, nrow=1), heights = c(0.5, 8))
-dev.off()
-
-com4 <- grid.arrange(pB, sur1_legend, heights = c(8, 1))
-comper <- grid.arrange(qa, qb, qc, shared_legend, heights = c(2,2,2,1))
-
-png(paste0(outdir, "all_sur_plot.png"),  width = 900, height = 900)
-grid.arrange(com4, comper, widths =c (1.2,3))
-dev.off()
-
 
 pB <- grid.arrange(
 grid.arrange(p1a$plot + theme(legend.position='hidden'),p1a$table, layout_matrix = rbind(c(1), c(1), c(2))),
@@ -128,10 +99,35 @@ grid.arrange(p3a$plot + theme(legend.position='hidden'),p3a$table, layout_matrix
 ncol = 1)
 
 
+### 07. Multiple plots
+source("Script/Functions/hh_vs_other.R")
+source("Script/Functions/other_vs_ll.R")
+source("Script/Functions/make_all_survival_plot.R")
 
-p <- grid.arrange(qa, qb, qc, shared_legend, heights = c(2,2,2,0.5))
 
-png(paste0(outdir, "supfig6.png"),  width = 600, height = 750)
-as_ggplot(p) +  draw_plot_label(label = c("a", "b", "c"), size = 15,
-                  x = c(0, 0, 0), y = c(1, 0.7, 0.4))   # transform to a ggplot
+com4 <- grid.arrange(pA, shared_legend, heights = c(8, 1))
+com4 <- grid.arrange(pB, sur1_legend, heights = c(8, 1))
+comper <- make_all_survival_plot(clinical_gse176307, clinical_imvigor210core, clinical_ucgenome)
+
+phh <- hh_vs_other(clinical_gse176307, clinical_imvigor210core, clinical_ucgenome)
+hh <- my_anno_plot("Both High vs. Any Low")
+
+pll <- other_vs_ll(clinical_gse176307, clinical_imvigor210core, clinical_ucgenome)
+ll <- my_anno_plot("Any High vs. Both Low")
+
+
+# hh vs. other / other vs. ll
+png(paste0(outdir, "Two_group_combination.png"),  width = 800, height = 900)
+grid.arrange(ggarrange(hh,ll,nrow =1), ggarrange(phh, pll, nrow=1), heights = c(0.5, 8))
 dev.off()
+
+# all survival plots
+png(paste0(outdir, "all_sur_plot.png"),  width = 900, height = 900)
+grid.arrange(com4, comper, widths =c (1.2,3))
+dev.off()
+
+#p <- grid.arrange(qa, qb, qc, shared_legend, heights = c(2,2,2,0.5))
+#png(paste0(outdir, "supfig6.png"),  width = 600, height = 750)
+#as_ggplot(p) +  draw_plot_label(label = c("a", "b", "c"), size = 15,
+#                  x = c(0, 0, 0), y = c(1, 0.7, 0.4))   # transform to a ggplot
+#dev.off()
