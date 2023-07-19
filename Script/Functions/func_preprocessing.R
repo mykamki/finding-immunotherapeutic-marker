91e60a58a439603edbd2b20dce698ae230168948
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-19 09:17:32 +0900

    Update func_preprocessing.R

--- Script/Functions/func_preprocessing.R
+++ Script/Functions/func_preprocessing.R
@@ -41,6 +41,34 @@ med <- function(data, indices) {
     resample <- data[indices]
     return(median(resample))
   }
-  
+
+
+zscore_transform <- function(x) {
+  (x-mean(x))/sd(x)
+}
+
+
+make_genesignature <- function(dataset, log_dataset) {
+
+	res2 <-  apply(log_dataset,2,mean)
+	sumgsig <- summary(res2)
+	names(res2)[which(res2<sumgsig[3])] -> low # low ID
+	names(res2)[which(res2>=sumgsig[3])] -> high # high ID
+
+	# Divide group
+	dataset<- dataset%>% mutate(Novel_Signature = ifelse(ID %in% high, "High", "Low"))
+	dataset$Novel_Signature <- factor(dataset$Novel_Signature)
+
+	if (identical(dataset$ID, names(res2))) {
+		dataset$Novel_Signature_score <- res2
+	} else {
+		res2 <- res2[dataset$ID]
+		dataset$Novel_Signature_score <- res2
+	}
+
+	return(dataset)
+}
+
+
 
 
