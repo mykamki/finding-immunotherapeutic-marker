c637df255f89b259ad9562ef8aca60cc10a5f507
mykamki <125241834+mykamki@users.noreply.github.com>
2023-07-19 14:22:18 +0900

    Update 10_combination_between_novel_baldder_signature_and_known_tmb.R

--- Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
+++ Script/10_combination_between_novel_baldder_signature_and_known_tmb.R
@@ -25,18 +25,6 @@ my_combination_survival_plot <- function(dataset, datasettitle) {
                 )
 }
 
-my_extreme_combination_survival_plot <- function(dataset, datasettitle) {
-	dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HL", "LL"))
-	fit1 <- survfit(Surv(time = time , event = status )~ Combination_Signature_TMB , data = dataset)
-	ggsurvplot(fit1, data = dataset, pval = T, pval.size =6, risk.table= T, 
-                   title= datasettitle, legend = "right",
-                   ggtheme=custom_theme(), tables.height = 0.1,
-                   tables.theme = theme_cleantable(), fontsize = 3, 
-                   risk.table.y.text.col = T, # colour risk table text annotations.
-                   risk.table.y.text = F, # show bars instead of names in text annotations
-                   #legend.labs = expression("Signature[High]", "Signature[Low]")  # Change legend labels
-                )
-}
 
 make_combination_group_dataset <- function(dataset) {
 	dataset <- dataset %>% filter(!is.na(tmb)) 
@@ -144,6 +132,46 @@ bar_plot_by_combination <- function(dataset) {
                 summarize(n = n())
         data$response.binary <- factor(data$response.binary, levels = c("R", "NR"))
 	
+	hh <- sum(data[data$Combination_Signature_TMB == "HH",]$n)
+	ll <- sum(data[data$Combination_Signature_TMB == "Any",]$n)
+
+	data$tt <- c(rep(hh,nrow(data[data$Combination_Signature_TMB == "HH",])), 
+		     rep(ll,nrow(data[data$Combination_Signature_TMB == "Any",])))
+	data <- data %>% mutate(percent = round(n/tt*100,1)) 
+
+	dt <- dcast(data, Combination_Signature_TMB ~ response.binary, value.var="n")
+	change_na2zero <- function(x) {ifelse(is.na(x), 0, as.double(x))}
+	apply(dt[,2:3], 2, change_na2zero) %>% fisher.test(simulate.p.value=TRUE,B=1e7) -> dtt
+
+        # make plot
+        p <- ggplot(data, aes(fill=response.binary, y=percent, x=Combination_Signature_TMB)) +
+                geom_col() + 
+                geom_signif(comparisons = list(c("HH", "Any")), 
+                            annotations = paste0("pvalue=", round(dtt$p.value,2)), y_position = 102) +
+                scale_x_discrete(labels=c(paste0("HH\n(n=", hh,")"), 
+					  paste0("Any\n(n=", ll,")"))) +
+                scale_fill_manual(values = c("orange", "royalblue3")) +
+                xlab("") + ylab("") + 
+                theme(panel.background = element_blank(),
+                     axis.ticks = element_blank(),
+                     axis.text.y = element_blank(),legend.position="top",
+                     plot.margin = margin(0, 1, 0, 1, "cm")) +
+                geom_text(aes(label=paste0(n," (",percent,"%)")), position = position_stack(vjust = 0.5)) +
+                guides(fill=guide_legend(title="Responder")) + 
+                theme(axis.text.x = element_text(size = 12))
+	
+	return(p)
+}
+
+
+bar_plot_by_combination <- function(dataset) {
+	#dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HH", "LL"))
+	data <- dataset %>% 
+                filter(!is.na(response.binary)) %>% 
+                group_by(Combination_Signature_TMB, response.binary) %>% 
+                summarize(n = n())
+        data$response.binary <- factor(data$response.binary, levels = c("R", "NR"))
+	
 	hh <- sum(data[data$Combination_Signature_TMB == "Any",]$n)
 	ll <- sum(data[data$Combination_Signature_TMB == "LL",]$n)
 
@@ -175,23 +203,7 @@ bar_plot_by_combination <- function(dataset) {
 	return(p)
 }
 
-make_combination_group_dataset <- function(dataset) {
-        dataset <- dataset %>% filter(!is.na(tmb)) 
-        tmb_med <- dataset %>% select(tmb) %>% as.matrix() %>% as.vector() %>% median()
-        dataset <- dataset %>% mutate(Known_TMB = ifelse(tmb >= tmb_med, "High", 
-                                                         ifelse(tmb < tmb_med, "Low", NA)))
-        dataset <- dataset %>% mutate(
-                Combination_Signature_TMB = case_when(
-                        Novel_Signature == "High" & Known_TMB == "High" ~ "HH",
-                        Novel_Signature == "High" & Known_TMB == "Low" ~ "HL",
-                        Novel_Signature == "Low" & Known_TMB == "High" ~ "LH",
-                        Novel_Signature == "Low" & Known_TMB == "Low" ~ "LL",
-                        TRUE ~ NA_character_
-                        )
-                )
-        #dataset$Combination_Signature_TMB <- factor(dataset$Combination_Signature_TMB, levels = c("Both High", "Any Low"))
-        return(dataset)
-}
+
 
 add_gene_exp <- function(log_dataset, dataset) {
 	genes <- c("SSR4", "RGS1", "HLA-DRB5", "APOE", "C1QB", "C1QA", "APOC1", "JCHAIN", "C1QC", "DERL3")
@@ -239,29 +251,29 @@ load(paste0(indir,"log_dataset_imvigor210core.RData"))
 load(paste0(indir,"log_dataset_ucgenome.RData"))
 
 ### 03. Survival relevance and ICB response of combination between novel bladder signature and known TMB in GSE176307
-clinical_gse176307 <- make_combination_group_dataset(clinical_gse176307)
-clinical_gse176307 <- make_binary_response_dataset(clinical_gse176307)
-p1a <- my_combination_survival_plot(clinical_gse176307, "GSE176307")
-p1b <- bar_plot_by_combination(clinical_gse176307)
+clinical_gse176307_2 <- make_combination_group_dataset(clinical_gse176307)
+clinical_gse176307_2 <- make_binary_response_dataset(clinical_gse176307_2)
+p1a <- my_combination_survival_plot(clinical_gse176307_2, "GSE176307")
+p1b <- bar_plot_by_combination(clinical_gse176307_2)
 
 
 ### 04. Survival relevance and ICB response of ncombination between novel bladder signature and known TMB in IMvigor210
-clinical_imvigor210core <- make_combination_group_dataset(clinical_imvigor210core)
-clinical_imvigor210core <- make_binary_response_dataset(clinical_imvigor210core)
-p2a <- my_combination_survival_plot(clinical_imvigor210core, "IMvigor210")
-p2b <- bar_plot_by_combination(clinical_imvigor210core)
+clinical_imvigor210core_2 <- make_combination_group_dataset(clinical_imvigor210core)
+clinical_imvigor210core_2 <- make_binary_response_dataset(clinical_imvigor210core_2)
+p2a <- my_combination_survival_plot(clinical_imvigor210core_2, "IMvigor210")
+p2b <- bar_plot_by_combination(clinical_imvigor210core_2)
 
 
 ### 05. Survival relevance and ICB response of combination between novel bladder signature and known TMB in UC-GENOME
-clinical_ucgenome <- make_combination_group_dataset(clinical_ucgenome)
-clinical_ucgenome <- make_binary_response_dataset(clinical_ucgenome)
-p3a <- my_combination_survival_plot(clinical_ucgenome, "UC-GENOME")
-p3b <- bar_plot_by_combination(clinical_ucgenome)
+clinical_ucgenome_2 <- make_combination_group_dataset(clinical_ucgenome)
+clinical_ucgenome_2 <- make_binary_response_dataset(clinical_ucgenome_2)
+p3a <- my_combination_survival_plot(clinical_ucgenome_2, "UC-GENOME")
+p3b <- bar_plot_by_combination(clinical_ucgenome_2)
 
 
-clinical_gse176307 <- add_gene_exp(log_dataset_gse176307, clinical_gse176307)
-clinical_imvigor210core <- add_gene_exp(log_dataset_imvigor210core, clinical_imvigor210core)
-clinical_ucgenome <- add_gene_exp(log_dataset_ucgenome, clinical_ucgenome)
+#clinical_gse176307 <- add_gene_exp(log_dataset_gse176307, clinical_gse176307)
+#clinical_imvigor210core <- add_gene_exp(log_dataset_imvigor210core, clinical_imvigor210core)
+#clinical_ucgenome <- add_gene_exp(log_dataset_ucgenome, clinical_ucgenome)
 
 ### 06. Plotting
 pA <- grid.arrange(
@@ -282,9 +294,28 @@ grid.arrange(pA, shared_legend, heights = c(8, 1))
 dev.off()
 
 
-png(paste0(outdir, "test.png"),  width = 400, height = 800)
-grid.arrange(pA, shared_legend, heights = c(8, 1))
+phh <- grid.arrange(pA, shared_legend, heights = c(8, 1))
+pll <- grid.arrange(pA, shared_legend, heights = c(8, 1))
+hh <- my_anno_plot("Both High vs. Any Low")
+ll <- my_anno_plot("Any High vs. Both Low")
+
+png(paste0(outdir, "test.png"),  width = 800, height = 900)
+grid.arrange(ggarrange(hh,ll,nrow =1), ggarrange(phh, pll, nrow=1), heights = c(0.5, 8))
 dev.off()
+
+com4 <- grid.arrange(pB, sur1_legend, heights = c(8, 1))
+comper <- grid.arrange(qa, qb, qc, shared_legend, heights = c(2,2,2,1))
+
+png(paste0(outdir, "all_sur_plot.png"),  width = 900, height = 900)
+grid.arrange(com4, comper, widths =c (1.2,3))
+dev.off()
+
+
+pB <- grid.arrange(
+grid.arrange(p1a$plot + theme(legend.position='hidden'),p1a$table, layout_matrix = rbind(c(1), c(1), c(2))),
+grid.arrange(p2a$plot + theme(legend.position='hidden'),p2a$table, layout_matrix = rbind(c(1), c(1), c(2))),
+grid.arrange(p3a$plot + theme(legend.position='hidden'),p3a$table, layout_matrix = rbind(c(1), c(1), c(2))),
+ncol = 1)
 
 my_HH_survival_plot <- function(dataset, datasettitle) {
 	dataset <- dataset %>% filter(Combination_Signature_TMB %in% c("HH", "LL"))
@@ -328,17 +359,17 @@ my_HL_survival_plot <- function(dataset, datasettitle) {
                 )
 }
 
-qa1 <- my_HH_survival_plot(clinical_gse176307, "")
-qa2 <- my_LH_survival_plot(clinical_gse176307, "")
-qa3 <- my_HL_survival_plot(clinical_gse176307, "")
+qa1 <- my_HH_survival_plot(clinical_gse176307_2, "")
+qa2 <- my_LH_survival_plot(clinical_gse176307_2, "")
+qa3 <- my_HL_survival_plot(clinical_gse176307_2, "")
 
-qb1 <- my_HH_survival_plot(clinical_imvigor210core, "")
-qb2 <- my_LH_survival_plot(clinical_imvigor210core, "")
-qb3 <- my_HL_survival_plot(clinical_imvigor210core, "")
+qb1 <- my_HH_survival_plot(clinical_imvigor210core_2, "")
+qb2 <- my_LH_survival_plot(clinical_imvigor210core_2, "")
+qb3 <- my_HL_survival_plot(clinical_imvigor210core_2, "")
 
-qc1 <- my_HH_survival_plot(clinical_ucgenome, "")
-qc2 <- my_LH_survival_plot(clinical_ucgenome, "")
-qc3 <- my_HL_survival_plot(clinical_ucgenome, "")
+qc1 <- my_HH_survival_plot(clinical_ucgenome_2, "")
+qc2 <- my_LH_survival_plot(clinical_ucgenome_2, "")
+qc3 <- my_HL_survival_plot(clinical_ucgenome_2, "")
 
 
 qa <- grid.arrange(
